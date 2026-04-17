28bd63b3b63b5a4c19bb71860a00d6ca93c9a169
filologtest-tech <umrbeksafarbayev@gmail.com>
2026-04-17 14:13:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.BOT_TOKEN;
 
+if (!token) {
+    console.error("BOT_TOKEN topilmadi!");
+    process.exit(1);
+}
+
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
+bot.on("polling_error", (error) => {
+    console.log("Polling error:", error.message);
+});
+
+bot.on("message", (msg) => {
     bot.sendMessage(msg.chat.id, "Ishlayapman ✅");
 });
 
